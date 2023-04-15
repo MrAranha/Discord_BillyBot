fdb53cc0c5291bc8a9f527146fdcf1648dcd7ce6
MrAranha <aranha.breno53@gmail.com>
2023-04-15 01:00:33 -0300

    v

---
+++ Modules/InteractionModule.cs
@@ -0,0 +1,12 @@
+﻿using Discord.Interactions;
+
+namespace BillyBosta_DiscordApp.Modules;
+
+public class InteractionModule : InteractionModuleBase<ShardedInteractionContext>
+{
+    [SlashCommand("echo", "Echo an input")]
+    public async Task Echo(string input)
+    {
+        await RespondAsync(input);
+    }
+}
