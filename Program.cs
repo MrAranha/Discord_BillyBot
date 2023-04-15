6a4b2e95c85a2ccabcbcba07b9a1a64c523bff53
MrAranha <aranha.breno53@gmail.com>
2023-04-14 23:26:54 -0300

    SEX

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using BillyBosta_DiscordApp;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
